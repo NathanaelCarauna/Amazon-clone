86d65bcc90ce4d2a8043bdb54a18866d8f85246f
NathanaelCarauna <nathanael.carauna@gmail.com>
2023-03-06 13:57:17 -0300

    Fix: move components to components folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Header from "./pages/Home/Header/Header";
+import Header from "./components/Header/Header";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route exact path="/"></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home/>
+              </>
+            }
+          ></Route>
         </Routes>
-        <Header />
-        <Home />
       </div>
     </BrowserRouter>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Home.css";
 import { headerItems, products } from "../../utils/ProductsData";
-import Slider from "../Slider/Slider";
-import Product from "../Product/Product";
+import Slider from "../../components/Slider/Slider";
+import Product from "../../components/Product/Product";
 import Banner1 from "../../BannerImages/Banner1.jpg";
 import Banner2 from "../../BannerImages/Banner2.jpg";
 import Banner3 from "../../BannerImages/Banner3.jpg";
 import Banner4 from "../../BannerImages/Banner4.jpg";
 import Banner5 from "../../BannerImages/Banner5.jpg";
 import Banner6 from "../../BannerImages/Banner6.jpg";
-import BackToTop from "../BackToTop/BackToTop";
+import BackToTop from "../../components/BackToTop/BackToTop";
 
 const Home = () => {
   const bannerImages = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6];
