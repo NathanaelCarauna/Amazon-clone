0dccdb816cf3134f4a9befcb5db99c7b0c924656
NathanaelCarauna <nathanael.carauna@gmail.com>
2023-03-06 12:05:31 -0300

    Feat: implement product and show products on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import { headerItems, products } from "../../utils/ProductsData";
 import Slider from "../Slider/Slider";
+import Product from "../Product/Product";
 import Banner1 from "../../BannerImages/Banner1.jpg";
 import Banner2 from "../../BannerImages/Banner2.jpg";
 import Banner3 from "../../BannerImages/Banner3.jpg";
@@ -19,11 +20,51 @@ const Home = () => {
       </div>
       <div className="home">
         <div className="home-container">
-          {/* Slider */}
-          <Slider images={bannerImages}/>
+          <Slider images={bannerImages} />
+          <div className="home-row">
+            {products.slice(0, 2).map((item) => (
+              <Product
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+                specification={item.specification}
+                detail={item.detail}
+              />
+            ))}
+          </div>
+          <div className="home-row">
+            {products.slice(2, 5).map((item) => (
+              <Product
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+                specification={item.specification}
+                detail={item.detail}
+              />
+            ))}
+          </div>
+          <div className="home-row">
+            {products.slice(5, 6).map((item) => (
+              <Product
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+                specification={item.specification}
+                detail={item.detail}
+              />
+            ))}
+          </div>
+          <div style={{ marginTop: "40px" }}>{/* back to top */}</div>
         </div>
-        <div className="home-row">{/* products */}</div>
-        <div style={{ marginTop: "40px" }}>{/* back to top */}</div>
       </div>
     </div>
   );
